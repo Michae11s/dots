5b8f205a1ac83cc8f1afa57b778ebd3f54e65951
Michae11s <kk9michaels@gmail.com>
2020-11-20 09:24:49 -0500

    Part of moving everything arround

---
+++ installp1.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#creates a standard barebones setup, run in standard live media
+
+#set date time
+timedatectl set-ntp true
+
+#do the partitioning
+echo "do partitioning now"
+echo "create /dev/sda1 as boot partition, /dev/sda2 as swap, /dev/sda3 as file system"
+echo "part type EF00=efi, EF02 as bios boot, part type 8200=swap"
+gdisk
+mkfs.fat -F32 /dev/sda1
+mkswap /dev/sda2
+mkfs.ext4 /dev/sda3
+swapon /dev/sda2
+mount /dev/sda3 /mnt
+mkdir /mnt/efi
+mount /dev/sda1 /mnt/efi 
+
+#pacstrap base base-devel
+pacstrap /mnt base base-devel linux linux-firmware vim git wget curl
+
+#gen fstab
+genfstab -U /mnt >> /mnt/etc/fstab
+
+cp -r root/* /mnt/
+cp pkglist /mnt/
+cp installp2.sh /mnt/
+
+arch-chroot /mnt
---
+++ installp2.sh
@@ -0,0 +1,57 @@
+#!/bin/bash
+#creates a standard barebones setup, run in standard live media
+
+
+#run inside chroot
+git clone https://github.com/Michae11s/dots.git
+cp /dots/etc/* /etc/
+
+#locale gen
+ln -sf /usr/share/zoneinfo/America/New_York /etc/localtime
+hwclock --systohc
+locale-gen
+
+#get the hostname
+echo "ENTER THE DESIRED HOSTNAME:"
+read HNAME
+echo $HNAME > /etc/hostname
+echo $HNAME >> /etc/hosts
+
+#run initramfs again
+mkinitcpio -P
+
+#cheange the root passwd
+echo "Change the root passwd"
+passwd
+
+### POST INSTALL CUSTOMIZATIONS ###
+# create a user
+echo "Enter desired user name:"
+read UNAME
+useradd -m -U --groups wheel,adm,rfkill,uucp -s /bin/bash $UNAME
+passwd $UNAME
+cd /home/$UNAME
+mkdir ./bin
+mkdir ./build
+mkdir ./Documents
+mkdir ./Downloads
+cp -r /dots/.config ./
+mv .config/bash/.bash_profile ./
+mv .config/bash/.bashrc ./
+rm -r .config/bash
+chown $UNAME:$UNAME -R /home/$UNAME
+
+#install suplimental packages
+pacman -Syu $(cat /dots/pkglist)
+
+#build auracle
+cd /home/$UNAME/build
+su $UNAME -c "git clone https://aur.archlinux.org/auracle-git.git"
+cd auracle-git
+su $UNAME -c "makepkg -si"
+
+#install bootloader
+mkdir /boot/grub
+grub-mkconfig -o /boot/grub/grub.cfg
+grub-install --target=x86_64-efi --efi-directory=/efi --bootloader-id=GRUB
+
