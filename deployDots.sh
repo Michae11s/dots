f3743e5b4387974ea1dd114cb3f3bb368c116647
Michaels@Rivendell <Michae11s>
2020-11-20 14:08:06 -0500

    added recursive delete for initial directory destruction

--- deployDots.sh
+++ deployDots.sh
@@ -2,7 +2,7 @@
 list="neofetch i3 polybar picom alacritty"
 #deploys dot files to where they need to go
 for i in $list; do
-	rm ~/.config/$i
+	rm -r ~/.config/$i
 	ln -s $(pwd)/.config/$i ~/.config/$i
 done
 
